ed685612c4669291e41240b56501b48a8d27e4bf
Daniel Young <Daniel.w.young.jr@gmail.com>
2023-05-16 11:43:27 -0500

    Implements up to, but not including, part 10

--- main.cpp
+++ main.cpp
@@ -57,8 +57,8 @@ int main() {
     
     auto materialGround = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto materialCenter = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto materialLeft = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto materialRight = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto materialLeft = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto materialRight = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     entities.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, materialGround));
     entities.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, materialCenter));
--- material.h
+++ material.h
@@ -31,17 +31,18 @@ public:
 
 class metal : public material {
 public:
-	metal(const color& albedo) : m_albedo(albedo) {}
+	metal(const color& albedo, double fuzz) : m_albedo(albedo), m_reflectionFuzz(fuzz < 1.0 ? fuzz : 1.0) {}
 
 	virtual bool scatter(
 		const ray& inRay, const point3& point, const vec3& normal, color& attenuation, ray& scattered
 	) const override {
 		vec3 reflected = reflect(unitVector(inRay.getDirection()), normal);
-		scattered = ray(point, reflected);
+		scattered = ray(point, reflected + m_reflectionFuzz * randomInUnitSphere());
 		attenuation = m_albedo;
 		return dot(scattered.getDirection(), normal) > 0.0;
 	}
 
 public:
 	color m_albedo;
+	double m_reflectionFuzz;
 };
