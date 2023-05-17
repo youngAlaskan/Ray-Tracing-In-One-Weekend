1feea526a2814eeb4a90d9100b1f9597b1d9ba95
Daniel Young <Daniel.w.young.jr@gmail.com>
2023-05-16 19:39:34 -0500

    Implements up to, but not including, part 13

--- main.cpp
+++ main.cpp
@@ -22,6 +22,54 @@
 //     Author : Daniel Young
 //     Version: Mar 23, 2023
 
+hittableList randomScene() {
+    hittableList entities;
+
+    auto groundMaterial = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    entities.add(make_shared<sphere>(point3(0.0, -1000.0, 0.0), 1000.0, groundMaterial));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = randomDouble();
+            point3 center(a + 0.9 * randomDouble(), 0.2, b + 0.9 * randomDouble());
+
+            if ((center - point3(4.0, 0.2, 0.0)).length() > 0.9) {
+                shared_ptr<material> sphereMaterial;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphereMaterial = make_shared<lambertian>(albedo);
+                    entities.add(make_shared<sphere>(center, 0.2, sphereMaterial));
+                }
+                else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = randomDouble(0, 0.5);
+                    sphereMaterial = make_shared<metal>(albedo, fuzz);
+                    entities.add(make_shared<sphere>(center, 0.2, sphereMaterial));
+                }
+                else {
+                    // glass
+                    sphereMaterial = make_shared<dielectric>(1.5);
+                    entities.add(make_shared<sphere>(center, 0.2, sphereMaterial));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    entities.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    entities.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    entities.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return entities;
+}
+
 color rayColor(const ray& r, const hittableList& entities, int depth) {
     hitRecord record;
 
@@ -46,33 +94,22 @@ int main() {
 
     // Image
 
-    const double aspectRatio = 16.0 / 9.0;
-    const int imageWidth = 400;
+    const double aspectRatio = 3.0 / 2.0;
+    const int imageWidth = 1200;
     const int imageHeight = static_cast<int>(double(imageWidth) / aspectRatio);
     const int samplesPerPixel = 100;
-    const int maxDepth = 50;
+    const int maxDepth = 20;
 
     // Entities
-    double R = cos(pi / 4.0);
-    hittableList entities;
-
-    auto materialGround = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto materialCenter = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto materialLeft = make_shared<dielectric>(1.5);
-    auto materialRight = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
-
-    entities.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, materialGround));
-    entities.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, materialCenter));
-    entities.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, materialLeft));
-    entities.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, materialLeft));
-    entities.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, materialRight));
-
+    
+    hittableList entities = randomScene();
+    
     // Camera
-    point3 lookFrom(3.0, 3.0, 2.0);
-    point3 lookAt(0.0, 0.0, -1.0);
+    point3 lookFrom(13.0, 2.0, 3.0);
+    point3 lookAt(0.0, 0.0, 0.0);
     vec3 vUp(0.0, 1.0, 0.0);
-    double focusDistance = (lookFrom - lookAt).length();
-    double aperture = 2.0;
+    double focusDistance = 10.0;
+    double aperture = 0.1;
     
     camera camera(lookFrom, lookAt, vUp, 20.0, aspectRatio, aperture, focusDistance);
 
